fa2a77fa0ccfc4cb172d9090be814ad3568196ed
slaykon84 <104734887+slaykon84@users.noreply.github.com>
2023-11-26 22:01:25 +0300

    new features

--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def insert_list(event):
             #insert modinfo output of the module to info text 
             info_text.insert(tk.END,"Size: " + str(getsize(selected_text)) + "MB \n\n") 
             info_text.insert(tk.END,modinfo(selected_text))
-            users_text.configure(state="normal")
             users_text.delete("1.0",tk.END)
             #insert users to users text and mark users of the module
          
@@ -31,7 +30,11 @@ def insert_list(event):
             users_text.insert(tk.END,get_users_for_module(selected_text,modules_list),"g_tag")
             users_text.insert(tk.END,"  |||  ")
             users_text.insert(tk.END,get_dep(modules_list,selected_text),"r_tag")
-            users_text.configure(state="disabled")
+
+def insselc():
+    selected_index = modules_list.curselection()
+    selected_text = modules_list.get(selected_index[0])
+    modprobe(selected_text,password)            
 
 def unins():
     selected_index = modules_list.curselection()
@@ -40,12 +43,22 @@ def unins():
         if rmmod(selected_text,password):
             insert_w()                  
             messagebox.showinfo("Info",f"{selected_text} was succesfuly uninserted.")
+def unins_m():
+    manuel_mods_name = simpledialog.askstring("Manualy Uninsert","Enter an module name or  multiple module names to uninsert, use space when entering multiple")
+    if manuel_mods_name:
+        if rmmod(manuel_mods_name,password):
+            if len(manuel_mods_name.split()) > 1:                  
+                messagebox.showinfo("Info",f"{manuel_mods_name} were succesfuly uninserted.")
+            else:
+                messagebox.showinfo("Info",f"{manuel_mods_name} was succesfuly uninserted.")
+
 
 def insrt():
-     manuel_mod_name = simpledialog.askstring("Manualy Insert","Enter an module name(s). Use space when inserting multiple.\nModules are often stored in /lib/modules/(your kernel version)/(file ending with .ko)").split()
+     manuel_mod_name = simpledialog.askstring("Manualy Insert","Enter an module name(s). Use space when inserting multiple.\nModules are often stored in /lib/modules/(your kernel version)/(file ending with .ko)")
      if manuel_mod_name:
       if modprobe(manuel_mod_name,password):
-          insert_w()     
+          insert_w()
+          res()    
 
 def insert_w():
     modules_list.delete(0, tk.END)
@@ -83,17 +96,21 @@ info_text.grid(row=0,column=2,padx=5,sticky="N", rowspan=20)
 
 users_text = tk.Text(window,width=30,height=2)
 users_text.grid(row=21, column=1, padx=5, pady=0, columnspan=2, rowspan=1, sticky='WE')
-users_text.configure(state="disabled")
+
 
 #insert list into listbox (had to put it in here because modules list was non-existent before)
 insert_w()
 
-rmmod_bt = tk.Button(window,text="Uninsert",command=unins,width=4)
+rmmod_bt = tk.Button(window,text="Uninsert",command=unins,width=5)
 rmmod_bt.grid(row=0,column=0,padx=5,sticky="N")
-admod_bt = tk.Button(window,text="Insert",command=insrt,width=4)
+admod_bt = tk.Button(window,text="Insert",command=insrt,width=5)
 admod_bt.grid(row=1,column=0,padx=5,sticky="N")
-lall = tk.Button(window, text="L. All", command=lambda:get_modules_from_lib(modules_list), width=4)
+lall = tk.Button(window, text="List All", command=lambda:get_modules_from_lib(modules_list), width=5)
 lall.grid(row=2,column=0,padx=5,sticky="N")
+inssel_b = tk.Button(window,text="Insert S.",width=5,command=insselc)
+inssel_b.grid(row=3,column=0,sticky="N")
+unins_mb = tk.Button(window,text="M. Unins.", width=5,command=unins_m)
+unins_mb.grid(row=4,column=0,sticky="N")
 rs = tk.Button(window,text="Reset List",width=5,command=reset)
 rs.grid(row=21,column=0,padx=5)
 
